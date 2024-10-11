31a5ada9c220be761cc3886849e085ed0fa8bf34
Merdan <wisew.tube@gmail.com>
2024-10-11 12:38:00 +0500

    server.go

---
+++ server.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = ":4000"
+
+func main() {
+	http.HandleFunc("/", ServeFiles)
+	http.HandleFunc("/script.js", JsFile)
+	http.HandleFunc("/style.css", CssFile)
+	fmt.Println("Serving @ ", "http://127.0.0.1" + port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func CssFile(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		path := r.URL.Path
+
+		fmt.Println(path)
+
+		if path == "/style.css"{
+			path = "./static/style.css"
+		} else {
+			path = "."+path
+		}
+
+		http.ServeFile(w, r, path)
+	default:
+		fmt.Fprintln(w, "Request type other than GET or POST not supported")
+	}
+}
+
+func JsFile(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		path := r.URL.Path
+
+		fmt.Println(path)
+
+		if path == "/script.js"{
+			path = "./static/script.js"
+		} else {
+			path = "."+path
+		}
+
+		http.ServeFile(w, r, path)
+	default:
+		fmt.Fprintln(w, "Request type other than GET or POST not supported")
+	}
+}
+
+func ServeFiles(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		path := r.URL.Path
+
+		fmt.Println(path)
+
+		if path == "/"{
+			path = "./static/index.html"
+		} else {
+			path = "."+path
+		}
+
+		http.ServeFile(w, r, path)
+	default:
+		fmt.Fprintln(w, "Request type other than GET or POST not supported")
+	}
+}
