9272183575b2a7217e189bf13d9c19188ef256f7
Merdan <wisew.tube@gmail.com>
2024-10-12 18:29:53 +0500

    server.go

--- server.go
+++ server.go
@@ -12,8 +12,25 @@ func main() {
 	http.HandleFunc("/", ServeFiles)
 	http.HandleFunc("/script.js", JsFile)
 	http.HandleFunc("/style.css", CssFile)
+	http.HandleFunc("/media/background.png", backgroundImg)
 	fmt.Println("Serving @ ", "http://127.0.0.1" + port)
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func backgroundImg(w http.ResponseWriter, r *http.Request) {
+	switch r.Method{
+	case "GET":
+		path := r.URL.Path
+		fmt.Println(path)
+		if path == "/" {
+			path = "./media/background.png"
+		} else {
+			path = "."+path
+		}
+		http.ServeFile(w, r, path)
+	default:
+			fmt.Fprintln(w, "Request type other than GET or POST not supported")
+	}
 }
 
 func CssFile(w http.ResponseWriter, r *http.Request) {
